47cf09c5ec569f192699170f9cf94349fda12a77
rzhon <rzwei@foxmail.com>
2017-12-03 13:40:36 +0800

    734. Sentence Similarity

---
+++ leetcode10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def areSentencesSimilar(self, words1, words2, pairs):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
+        """
+        # 734. Sentence Similarity
+        if len(words1) != len(words2):
+            return False
+        d = {}
+        for l, r in pairs:
+            if l not in d:
+                d[l] = {r}
+            else:
+                d[l].add(r)
+            if r not in d:
+                d[r] = {l}
+            else:
+                d[r].add(l)
+        for w1, w2 in zip(words1, words2):
+            if w1 == w2 or w1 in d.get(w2, []):
+                continue
+            return False
+        return True
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.areSentencesSimilar(['great', 'acting', 'skills'], ['fine', 'drama', 'talent'],
+                                  [["great", "fine"], ["acting", "drama"], ["skills", "talent"]]))
