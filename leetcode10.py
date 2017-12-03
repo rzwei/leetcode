6bc50f51da5115c27609ab5018f741190d4f3857
rzhon <rzwei@foxmail.com>
2017-12-03 23:23:46 +0800

    MyCalendar

--- leetcode10.py
+++ leetcode10.py
@@ -1,19 +1,54 @@
+import bisect
+
+
+# class TreeNode:
+#     def __init__(self, s, e):
+#         self.start = s
+#         self.end = e
+#         self.right = None
+#         self.left = None
+#
+#
+# class MyCalendar:
+#     def __init__(self):
+#         self.times = {}
+#         self.books = TreeNode(-1, -1)
+#
+#     def search(self, p, s, e):
+#         if p.start >= e:
+#             if not p.left:
+#                 p.left = TreeNode(s, e)
+#                 return True
+#             else:
+#                 return self.search(p.left, s, e)
+#         if p.end <= s:
+#             if not p.right:
+#                 p.right = TreeNode(s, e)
+#                 return True
+#             else:
+#                 return self.search(p.right, s, e)
+#         return False
+#
+#     def book(self, start, end):
+#         """
+#         :type start: int
+#         :type end: int
+#         :rtype: bool
+#         """
+#         return self.search(self.books, start, end)
 class MyCalendar:
     def __init__(self):
-        self.times = {}
+        self.start = []
+        self.end = []
 
     def book(self, start, end):
-        """
-        :type start: int
-        :type end: int
-        :rtype: bool
-        """
-        for k, v in self.times.items():
-            if end <= k or start >= v:
-                continue
-            return False
-        self.times[start] = end
-        return True
+        i = bisect.bisect_right(self.end, start)
+        j = bisect.bisect_left(self.start, end)
+        if i == j:
+            self.start.index(i, start)
+            self.end.index(i, end)
+            return True
+        return False
 
 
 class Solution:
@@ -52,4 +87,4 @@ if __name__ == '__main__':
     for v1, v2 in [[97, 100], [33, 51], [89, 100], [83, 100], [75, 92], [76, 95], [19, 30], [53, 63], [8, 23], [18, 37],
                    [87, 100], [83, 100], [54, 67], [35, 48], [58, 75], [70, 89], [13, 32], [44, 63], [51, 62], [2, 15]]:
         print(calendar.book(v1, v2))
-    print(calendar.times.items())
+    # print(calendar.times.items())
