359d7e495b63fac7588750178720645e869a7d7f
rzhon <rzwei@foxmail.com>
2017-12-16 11:08:32 +0800

    218. The Skyline Problem

--- leetcode10.py
+++ leetcode10.py
@@ -1368,8 +1368,14 @@ class Solution:
         if j == Len:
             return s
         suffix = s[j:]
-        # print(suffix[::-1], self.shortestPalindrome(s[:j]), suffix)
         return suffix[::-1] + self.shortestPalindrome(s[:j]) + suffix
+
+    def getSkyline(self, buildings):
+        """
+        :type buildings: List[List[int]]
+        :rtype: List[List[int]]
+        218. The Skyline Problem
+        """
 
 
 if __name__ == '__main__':
