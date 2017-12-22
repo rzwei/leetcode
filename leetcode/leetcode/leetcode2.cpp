c6e16013d20d4582ee58bca1a2ecc61d885c3dff
任仲蔚 <rzhongwei@hotmail.com>
2017-12-22 19:10:16 +0800

    363. Max Sum of Rectangle No Larger Than K

--- leetcode/leetcode/leetcode2.cpp
+++ leetcode/leetcode/leetcode2.cpp
@@ -12,11 +12,35 @@
 #include <map>
 #include <queue>
 using namespace std;
-class
+class Solution
 {
 public:
-	vector<int> countSmaller(vector<int>& nums) {
-
+	int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+		if (matrix.empty())
+			return 0;
+		int row = matrix.size(), col = matrix[0].size();
+		int res = -2147483648;
+		for (int l = 0; l < col; l++)
+		{
+			vector<int>sums(row, 0);
+			for (int r = l; r < col; r++) {
+				for (int i = 0; i < row; i++)
+					sums[i] += matrix[i][r];
+				set<int>accSet;
+				accSet.insert(0);
+				int curMax = -2147483648, curSum = 0;
+				for (int sum : sums)
+				{
+					curSum += sum;
+					auto it = accSet.lower_bound(curSum - k);
+					if (it != accSet.end())
+						curMax = max(curMax, curSum - *it);
+					accSet.insert(curSum);
+				}
+				res = max(res, curMax);
+			}
+		}
+		return res;
 	}
 };
 
