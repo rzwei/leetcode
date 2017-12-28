214d8abe847d29fd45170c0b19c6663240e21a1a
任仲蔚 <rzhongwei@hotmail.com>
2017-12-28 16:34:48 +0800

    514. Freedom Trail -- dfs

--- leetcode-clion/leetcode.cpp
+++ leetcode-clion/leetcode.cpp
@@ -4,6 +4,7 @@
 //#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <map>
 #include <set>
 #include <queue>
@@ -49,12 +50,38 @@ public:
         }
         return dp[0][0] + m;
     }
+
+    int dfs_findRotateSteps(string &ring, string &key, int i, int j, map<pair<int, int>, int> &memo) {
+        if (memo.find(make_pair(i, j)) != memo.end()) {
+            return memo[make_pair(i, j)];
+        }
+//        cout << i << " " << j << endl;
+        if (i == key.length()) {
+            return key.length();
+        }
+        int diff, step;
+        int ret = INT32_MAX;
+        for (int k = 0; k < ring.length(); k++) {
+            if (ring[k] == key[i]) {
+                diff = abs(k - j);
+                step = diff < ring.length() - diff ? diff : ring.length() - diff;
+                ret = min(ret, dfs_findRotateSteps(ring, key, i + 1, k, memo) + step);
+            }
+        }
+        memo[make_pair(i, j)] = ret;
+        return ret;
+    }
+
+    int findRotateSteps_dfs(string ring, string key) {
+        map<pair<int, int>, int> memo;
+        return dfs_findRotateSteps(ring, key, 0, 0, memo);
+    }
 };
 
 int main() {
 //    cout << "hello world!" << endl;
     Solution sol;
-    cout << sol.findRotateSteps("godding", "gd") << endl;
+    cout << sol.findRotateSteps_dfs("godding", "gd") << endl;
 //    vector<int> nums{1, 3, -1, -3, 5, 3, 6, 7};
 //    auto r = sol.medianSlidingWindow(nums, 3);
 //    for (double i:r) {
