29313c1ee28f3292d338b6acfe8a2d544c94da0c
任仲蔚 <rzhongwei@hotmail.com>
2017-12-28 13:00:48 +0800

    add leetcode.cpp for clion

--- leetcode-clion/leetcode.cpp
+++ leetcode-clion/leetcode.cpp
@@ -10,7 +10,7 @@ public:
     vector<double> medianSlidingWindow(vector<int> &nums, int k) {
         multiset<int> window(nums.begin(), nums.begin() + k);
         auto mid = next(window.begin(), k / 2);
-        vector<double> medians;
+        vector<double> medians  ;
         for (int i = k;; i++) {
             medians.push_back((double(*mid) + *prev(mid, 1 - k % 2)) / 2);
             if (i == nums.size())
