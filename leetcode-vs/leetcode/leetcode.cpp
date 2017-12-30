402183490d5b8da6580646e7274385627bb0dfb2
任仲蔚 <rzhongwei@hotmail.com>
2017-12-30 15:54:04 +0800

    667. Beautiful Arrangement II

--- leetcode-vs/leetcode/leetcode.cpp
+++ leetcode-vs/leetcode/leetcode.cpp
@@ -308,6 +308,18 @@ public:
         return vector<int>(arr.begin() + lo, arr.begin() + lo + k);
     }
 
+    //667. Beautiful Arrangement II
+    vector<int> constructArray(int n, int k) {
+        vector<int> res;
+        for (int i = 1, j = n; i <= j;) {
+            if (k > 1) {
+                res.push_back(k-- % 2 ? i++ : j--);
+            } else {
+                res.push_back(i++);
+            }
+        }
+        return res;
+    }
 };
 
 int main() {
