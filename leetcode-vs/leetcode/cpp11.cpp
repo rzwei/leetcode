41fd0fee4231fa518d297bbf81c4d14a799db94b
rzhon <rzwei@foxmail.com>
2018-01-15 23:21:03 +0800

    null

---
+++ leetcode-vs/leetcode/cpp11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <sstream>
+#include <climits>
+using namespace std;
+
+
+
+int main_()
+{
+
+}
--- leetcode-vs/leetcode/leetcode3.cpp
+++ leetcode-vs/leetcode/leetcode3.cpp
@@ -13,6 +13,35 @@
 #include <stack>
 
 using namespace std;
+class AllOne {
+public:
+	/** Initialize your data structure here. */
+	AllOne() {
+
+	}
+
+	/** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
+	void inc(string key) {
+
+	}
+
+	/** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
+	void dec(string key) {
+
+	}
+
+	/** Returns one of the keys with maximal value. */
+	string getMaxKey() {
+
+	}
+
+	/** Returns one of the keys with Minimal value. */
+	string getMinKey() {
+
+	}
+};
+
+
 class LFUCache {
 public:
 	unordered_map<int, int> m;
@@ -120,8 +149,8 @@ public:
 		for (auto&i : tickets)
 			m[i.first].push_back(i.second);
 		vector<string> path;
-		visit_findItinerary(path, "JFK", m);
-		reverse(path.begin(),path.end());
+		visit_findItinerary(path, "JFK", / s m);
+		reverse(path.begin(), path.end());
 		return path;
 	}
 };
