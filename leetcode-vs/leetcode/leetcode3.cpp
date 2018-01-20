c254aa535b4e174bf7b20ce0ae2ef061de00b421
rzwei <rzhongwei@hotmail.com>
2018-01-20 15:18:16 +0800

    730. Count Different Palindromic Subsequences

--- leetcode-vs/leetcode/leetcode3.cpp
+++ leetcode-vs/leetcode/leetcode3.cpp
@@ -492,7 +492,7 @@ public:
     }
 
     int dp_691(string &target, vector<vector<int>> &stickers, map<string, int> &memo) {
-        cout << target << endl;
+//        cout << target << endl;
         if (memo.find(target) != memo.end()) {
             return memo[target];
         }
@@ -522,7 +522,7 @@ public:
                         j++;
                     }
                 }
-                cout << "use " << i << " ";
+//                cout << "use " << i << " ";
                 int r = dp_691(newtarget, stickers, memo);
                 if (r != -1) {
                     res = min(res, r + 1);
@@ -637,11 +637,47 @@ public:
         }
         return 0;
     }
+
+    //730. Count Different Palindromic Subsequences
+    int countPalindromicSubsequences(string s) {
+        int md = 1000000007;
+        int n = s.size();
+        int dp[3][n][4];
+        for (int len = 1; len <= n; ++len) {
+            for (int i = 0; i + len <= n; ++i)
+                for (int x = 0; x < 4; ++x) {
+                    int &ans = dp[2][i][x];
+                    ans = 0;
+                    int j = i + len - 1;
+                    char c = 'a' + x;
+                    if (len == 1) ans = s[i] == c;
+                    else {
+                        if (s[i] != c) ans = dp[1][i + 1][x];
+                        else if (s[j] != c) ans = dp[1][i][x];
+                        else {
+                            ans = 2;
+                            if (len > 2)
+                                for (int y = 0; y < 4; ++y) {
+                                    ans += dp[0][i + 1][y];
+                                    ans %= md;
+                                }
+                        }
+                    }
+                }
+            for (int i = 0; i < 2; ++i)
+                for (int j = 0; j < n; ++j)
+                    for (int x = 0; x < 4; ++x)
+                        dp[i][j][x] = dp[i + 1][j][x];
+        }
+        int ret = 0;
+        for (int x = 0; x < 4; ++x) ret = (ret + dp[2][0][x]) % md;
+        return ret;
+    }
 };
 
 int main() {
     Solution sol;
-    cout << sol.smallestGoodBase("13") << endl;
+    cout << sol.smallestGoodBase("26546") << endl;
 //    cout << sol.predictPartyVictory("RD") << endl;
 //    cout << sol.predictPartyVictory("D") << endl;
 //    cout << sol.predictPartyVictory("RDD") << endl;
