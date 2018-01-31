63e9425f4fa1dc03759da0aae6242f7caa912bde
rzwei <rzhongwei@hotmail.com>
2018-01-31 18:54:17 +0800

    564. Find the Closest Palindrome

--- leetcode-vs/leetcode/leetcode4.cpp
+++ leetcode-vs/leetcode/leetcode4.cpp
@@ -1050,17 +1050,48 @@ public:
 		}
 		return candB;
 	}
+
+	//564. Find the Closest Palindrome
+	string nearestPalindromic(string n) {
+		int l = n.size();
+		set<long> candidates;
+		// biggest, one more digit, 10...01
+		candidates.insert(long(pow(10, l)) + 1);
+		// smallest, one less digit, 9...9 or 0
+		candidates.insert(long(pow(10, l - 1)) - 1);
+		// the closest must be in middle digit +1, 0, -1, then flip left to right
+		long prefix = stol(n.substr(0, (l + 1) / 2));
+		for (long i = -1; i <= 1; ++i) {
+			string p = to_string(prefix + i);
+			string pp = p + string(p.rbegin() + (l & 1), p.rend());
+			candidates.insert(stol(pp));
+		}
+		long num = stol(n), minDiff = LONG_MAX, diff, minVal;
+		candidates.erase(num);
+		for (long val : candidates) {
+			diff = abs(val - num);
+			if (diff < minDiff) {
+				minDiff = diff;
+				minVal = val;
+			}
+			else if (diff == minDiff) {
+				minVal = min(minVal, val);
+			}
+		}
+		return to_string(minVal);
+	}
 };
 
 
 int main() {
 	Solution sol;
-	vector<vector<int > >edges{ { 1,2 },{ 1,3 },{ 2,3 } };
+	cout << sol.nearestPalindromic("1283") << endl;
+	//vector<vector<int > >edges{ { 1,2 },{ 1,3 },{ 2,3 } };
 	//edges = { { 1,2 },{ 2,3 },{ 3,4 },{ 4,1 },{ 1,5 } };
-	auto r = sol.findRedundantDirectedConnection(edges);
-	for (auto i : r)
-		cout << i << " ";
-	cout << endl;
+	//auto r = sol.findRedundantDirectedConnection(edges);
+	//for (auto i : r)
+	//	cout << i << " ";
+	//cout << endl;
 	//cout << sol.largestPalindrome(3) << endl;
 	//for (int i = 1; i <= 8; i++)
 	//	cout << sol.largestPalindrome(i) << endl;
