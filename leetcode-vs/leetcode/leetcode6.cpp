47d2d6634043811c73a7898a0bc7c4fa0dfd655d
rzwei <rzhongwei@hotmail.com>
2018-04-29 11:18:42 +0800

    Weekly Contest 82

--- leetcode-vs/leetcode/leetcode6.cpp
+++ leetcode-vs/leetcode/leetcode6.cpp
@@ -131,6 +131,149 @@ public:
         }
         return ans;
     }
+
+    //825. Friends Of Appropriate Ages
+    int numFriendRequests(vector<int> &ages) {
+        int ans = 0;
+        vector<int> nums(120 + 1);
+        for (auto n:ages) {
+            nums[n] += 1;
+        }
+        for (int a = 1; a <= 120; ++a) {
+            for (int b = 1; b <= 120; ++b) {
+                if (2 * b - 14 <= a || b > a || b > 100 && a < 100) {
+                    continue;
+                }
+                if (a == b) {
+                    ans += nums[a] * (nums[a] - 1);
+                } else {
+                    ans += nums[a] * nums[b];
+                }
+            }
+        }
+        return ans;
+    }
+
+    //826. Most Profit Assigning Work
+    int maxProfitAssignment(vector<int> &difficulty, vector<int> &profit, vector<int> &worker) {
+        int len = difficulty.size();
+        if (len == 0) {
+            return 0;
+        }
+        vector<int> dp(len);
+        vector<pair<int, int>> nums(len);
+        for (int i = 0; i < len; ++i) {
+            nums[i] = {difficulty[i], -profit[i]};
+        }
+        sort(nums.begin(), nums.end());
+        dp[0] = -nums[0].second;
+        for (int i = 1; i < len; ++i) {
+            dp[i] = max(dp[i - 1], -nums[i].second);
+        }
+        int ans = 0;
+        sort(worker.begin(), worker.end());
+        int j = worker.size() - 1, i = len - 1;
+
+        while (j >= 0) {
+            while (i >= 0 && nums[i].first > worker[j]) {
+                --i;
+            }
+            if (i < 0) break;
+            ans += dp[i];
+            j -= 1;
+        }
+        return ans;
+    }
+
+    int dfs_island(int x, int y, vector<vector<int>> &grid, int idx) {
+        static vector<vector<int>> dirs = {{0,  1},
+                                           {0,  -1},
+                                           {1,  0},
+                                           {-1, 0}};
+        int m = grid[0].size(), n = grid[1].size();
+        grid[x][y] = idx;
+        int cnt = 1;
+        for (auto &d:dirs) {
+            int nx = x + d[0], ny = y + d[1];
+            if (0 <= nx && nx < m && 0 <= ny && ny < n && grid[nx][ny] == 1) {
+                cnt += dfs_island(nx, ny, grid, idx);
+            }
+        }
+        return cnt;
+    }
+
+    //827. Making A Large Island
+    int largestIsland(vector<vector<int>> &grid) {
+        int m = grid[0].size(), n = grid[1].size();
+        unordered_map<int, int> cnt;
+        int idx = -1;
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    int t = dfs_island(i, j, grid, idx);
+                    cnt[idx] = t;
+                    ans = max(ans, t);
+                    --idx;
+                }
+            }
+        }
+        vector<vector<int>> dirs = {{0,  1},
+                                    {0,  -1},
+                                    {1,  0},
+                                    {-1, 0}};
+        unordered_set<int> cc;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) {
+                    cc.clear();
+                    for (auto &d:dirs) {
+                        int nx = i + d[0], ny = j + d[1];
+                        if (0 <= nx && nx < m && 0 <= ny && ny < n) {
+                            if (grid[nx][ny] < 0) {
+                                cc.insert(grid[nx][ny]);
+                            }
+                        }
+                    }
+                    int t = 1;
+                    for (auto cx:cc) {
+                        t += cnt[cx];
+                    }
+                    ans = max(ans, t);
+                }
+            }
+        }
+        return ans;
+    }
+
+    bool ck(char ch) {
+        string v = "aeiouAEIOU";
+        return (v.find(ch) != string::npos);
+    }
+
+    //824. Goat Latin
+    string toGoatLatin(string S) {
+        istringstream is(S);
+        vector<string> res;
+        string s;
+        string A = "";
+        while (is >> s) {
+            A += "a";
+            string t = "";
+            if (ck(s[0])) {
+                t = s + "ma";
+            } else {
+                t = s.substr(1) + s[0] + "ma";
+            }
+            t += A;
+            res.push_back(t);
+        }
+        string ans = res[0];
+        for (int i = 1; i < (int) res.size(); i++) {
+            ans += " " + res[i];
+        }
+        return ans;
+    }
 };
 
 int main() {
