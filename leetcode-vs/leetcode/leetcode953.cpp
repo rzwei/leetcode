6e98ef4a28dd40a5d295532beb3bad1568b05eb7
rzhon <974337380@qq.com>
2019-02-17 10:38:43 +0800

    991. Broken Calculator

--- leetcode-vs/leetcode/leetcode953.cpp
+++ leetcode-vs/leetcode/leetcode953.cpp
@@ -1145,6 +1145,13 @@ public:
 		}
 		return ans;
 	}
+
+	//991. Broken Calculator
+	int brokenCalc(int X, int Y) {
+		if (X >= Y) return X - Y;
+		if (Y & 1) return 1 + brokenCalc(X, Y + 1);
+		return 1 + brokenCalc(X, Y / 2);
+	}
 };
 
 int main()
