0d2a3d1e752b31e1027d3688d5ccbc8df283e6e1
rzwei <ren@DESKTOP-5LDI1V5.localdomain>
2019-09-12 00:15:58 +0800

    1187. Make Array Strictly Increasing

--- leetcode-vs/leetcode/leetcode1114.cpp
+++ leetcode-vs/leetcode/leetcode1114.cpp
@@ -592,6 +592,53 @@ public:
 	}
 };
 
+struct hash_pair { 
+    template <class T1, class T2> 
+    size_t operator()(const pair<T1, T2>& p) const
+    { 
+        auto hash1 = hash<T1>{}(p.first); 
+        auto hash2 = hash<T2>{}(p.second); 
+        return hash1*37 + hash2; 
+    } 
+}; 
+
+/*
+//1187. Make Array Strictly Increasing
+class Solution {
+    unordered_map<pair<int, int>, int, hash_pair> memo;
+    public:
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        sort(arr2.begin(), arr2.end());
+        int val= dfs(arr1, arr2, 0, -1);
+        if(val>=3000) return -1;
+        else return val;
+    }
+
+    int dfs(vector<int> & arr1, vector<int> & arr2, int i, int prev) {
+        if(i == arr1.size())
+            return 0;
+        if(memo.count(make_pair(i, prev)))
+            return memo[make_pair(i, prev)];
+        int res = 0;
+        int j = upper_bound(arr2.begin(), arr2.end(), prev) - arr2.begin();
+
+        if(arr1[i] <= prev) {
+            if(j >= arr2.size()) {
+                res = 3000;
+            } else {
+                res = 1 + dfs(arr1, arr2, i+1, arr2[j]);
+            }
+        } else {
+            if(j >= arr2.size() || arr2[j] >= arr1[i]) {
+                res = dfs(arr1, arr2, i+1, arr1[i]);
+            } else {
+                res = min(1+dfs(arr1, arr2, i+1, arr2[j]), dfs(arr1, arr2, i+1, arr1[i]));
+            };
+        }
+        return memo[make_pair(i, prev)] = res;
+    }
+};
+*/
 class Solution
 {
 public:
