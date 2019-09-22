938c5deed225d91abd0b6bbaca44de89709f9ce0
rzwei <rzwei@foxmail.com>
2019-09-22 14:48:51 +0800

    //1202. Smallest String With Swaps

--- leetcode-vs/leetcode/leetcode1192.cpp
+++ leetcode-vs/leetcode/leetcode1192.cpp
@@ -1,3 +1,4 @@
+#include <numeric>
 #include <future>
 #include <stack>
 #include <unordered_map>
@@ -15,7 +16,7 @@
 #include <mutex>
 #include <string>
 using namespace std;
-
+typedef long long ll;
 /*
 typedef pair<int, int> ii;
 const int N = 1e5 + 10;
@@ -151,6 +152,172 @@ public:
 class Solution
 {
 public:
+	//1196. How Many Apples Can You Put into the Basket
+	int maxNumberOfApples(vector<int>& a) {
+		int n = a.size();
+		sort(a.begin(), a.end());
+		int u = 0;
+		int const maxn = 5000;
+		int ans = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			u += a[i];
+			if (u > maxn) break;
+			ans++;
+		}
+		return ans;
+	}
+
+	//1197. Minimum Knight Moves
+	int minKnightMoves(int X, int Y) {
+		vector <int> dr = { -2, -2, -1, 1, 2, 2, 1, -1 };
+		vector <int> dc = { 1, -1, -2, -2, -1, 1, 2, 2 };
+		X = abs(X);
+		Y = abs(Y);
+		int x = min(X, Y);
+		int y = max(X, Y);
+
+		if (x == 0 && y == 0) {
+			return 0;
+		}
+		if (x == 0 && y == 2) {
+			return 2;
+		}
+		if (x == 1 && y == 1) {
+			return 2;
+		}
+		if (x == 1 && y == 3) {
+			return 2;
+		}
+		if (x == 3 && y == 4) {
+			return 3;
+		}
+		return 1 + minKnightMoves(x - 1, y - 2);
+	}
+
+	//1198. Find Smallest Common Element in All Rows
+	int smallestCommonElement(vector<vector<int>>& a) {
+		int n = a.size();
+		int m = a[0].size();
+		for (int i = 0; i < m; ++i)
+		{
+			bool f = true;
+			for (int j = 1; f && j < n; ++j)
+			{
+				auto it = lower_bound(a[j].begin(), a[j].end(), a[0][i]);
+				if (it == a[j].end() || *it != a[0][i])
+				{
+					f = false;
+				}
+			}
+			if (f) return a[0][i];
+		}
+		return -1;
+	}
+
+	//1200. Minimum Absolute Difference
+	vector<vector<int>> minimumAbsDifference(vector<int>& a) {
+		int n = a.size();
+		sort(a.begin(), a.end());
+		int diff = INT_MAX;
+		for (int i = 1; i < n; ++i)
+		{
+			diff = min(diff, a[i] - a[i - 1]);
+		}
+		vector<vector<int>> ans;
+		for (int i = 1; i < n; ++i)
+		{
+			if (a[i] - a[i - 1] == diff)
+			{
+				ans.push_back({ a[i - 1], a[i] });
+			}
+		}
+		return ans;
+	}
+
+	ll check(ll n, ll a, ll b, ll c)
+	{
+		ll ans = 0;
+		ans += n / a;
+		ans += n / b;
+		ans += n / c;
+		ans -= n / lcm(a, b);
+		ans -= n / lcm(a, c);
+		ans -= n / lcm(b, c);
+		ans += n / lcm(lcm(a, b), c);
+		return ans;
+	}
+
+	//1201. Ugly Number III
+	int nthUglyNumber(int n, int a, int b, int c) {
+		ll l = 1, r = 2 * 10e9;
+		while (l < r)
+		{
+			ll m = (l + r) / 2;
+			ll tot = check(m, a, b, c);
+			if (tot >= n)
+			{
+				r = m;
+			}
+			else
+			{
+				l = m + 1;
+			}
+		}
+		return l;
+	}
+
+	void dfs_1202(int u, vector<vector<int>>& g, vector<int>& vis, int color)
+	{
+		if (vis[u]) return;
+		vis[u] = color;
+		for (auto& v : g[u])
+		{
+			dfs_1202(v, g, vis, color);
+		}
+	}
+	//1202. Smallest String With Swaps
+	string smallestStringWithSwaps(string s, vector<vector<int>>& a) {
+		int n = s.size();
+		vector<vector<int>> g(n);
+		for (auto& e : a)
+		{
+			int u = e[0], v = e[1];
+			g[u].push_back(v);
+			g[v].push_back(u);
+		}
+		int color = 1;
+		vector<int> vis(n);
+		for (int i = 0; i < n; ++i)
+		{
+			if (vis[i] == 0)
+			{
+				dfs_1202(i, g, vis, color++);
+			}
+		}
+		vector<vector<int>> index(color);
+		vector<vector<int>> value(color);
+		for (int i = 0; i < n; ++i)
+		{
+			int c = vis[i];
+			index[c].push_back(i);
+			value[c].push_back(s[i]);
+		}
+		for (auto& e : value)
+		{
+			sort(e.begin(), e.end());
+		}
+		for (int i = 0; i < index.size(); ++i)
+		{
+			for (int j = 0; j < index[i].size(); ++j)
+			{
+				int idx = index[i][j];
+				char val = value[i][j];
+				s[idx] = val;
+			}
+		}
+		return s;
+	}
 };
 
 int main()
