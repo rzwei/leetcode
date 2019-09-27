88d733b020b7c67591366eb2af12b63d557b4e5c
rzwei <rzwei@foxmail.com>
2019-09-27 18:51:17 +0800

    1199. Minimum Time to Build Blocks

--- leetcode-vs/leetcode/leetcode1192.cpp
+++ leetcode-vs/leetcode/leetcode1192.cpp
@@ -318,6 +318,22 @@ public:
 		}
 		return s;
 	}
+
+	//1199. Minimum Time to Build Blocks
+	int minBuildTime(vector<int>& blocks, int split) {
+		priority_queue<int> pq;
+		for (int b : blocks) pq.push(-b);
+		while (pq.size() > 1)
+		{
+			int a = -pq.top();
+			pq.pop();
+			int b = -pq.top();
+			pq.pop();
+			pq.push(-(split + max(a, b)));
+		}
+		return -pq.top();
+	}
+
 };
 
 int main()
