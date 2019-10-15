770e207485d83be6a33024cca3dcbddb6523a9dd
rzwei <rzwei@foxmail.com>
2019-10-15 11:10:03 +0800

    1223. Dice Roll Simulation

--- leetcode-vs/leetcode/leetcode1192.cpp
+++ leetcode-vs/leetcode/leetcode1192.cpp
@@ -795,6 +795,33 @@ public:
 		return ans;
 	}
 
+	//1223. Dice Roll Simulation
+	int dieSimulator(int n, vector<int> rollMax) {
+		long divisor = (long)pow(10, 9) + 7;
+		vector<vector<long long>> dp(n, vector<long long>(7));
+		for (int i = 0; i < 6; i++) {
+			dp[0][i] = 1;
+		}
+		dp[0][6] = 6;
+		for (int i = 1; i < n; i++) {
+			long sum = 0;
+			for (int j = 0; j < 6; j++) {
+				dp[i][j] = dp[i - 1][6];
+				if (i - rollMax[j] < 0) {
+					sum = (sum + dp[i][j]) % divisor;
+				}
+				else {
+					if (i - rollMax[j] - 1 >= 0) dp[i][j] = (dp[i][j] - (dp[i - rollMax[j] - 1][6] - dp[i - rollMax[j] - 1][j])) % divisor + divisor;
+					else dp[i][j] = (dp[i][j] - 1) % divisor;
+					sum = (sum + dp[i][j]) % divisor;
+				}
+
+			}
+			dp[i][6] = sum;
+		}
+		return (int)(dp[n - 1][6]);
+	}
+
 	//1224. Maximum Equal Frequency
 	int maxEqualFreq(vector<int>& a) {
 		class FreqMap
